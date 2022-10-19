bd4a5104e221419d166444f6e75edfc913403635
Jay Chiddarwar <jchiddar@asu.edu>
2022-10-19 01:19:30 -0700

    added instruction to run and replace files in Readme

--- PTBS/src/Facade.java
+++ PTBS/src/Facade.java
@@ -28,53 +28,61 @@ public class Facade {
 	public void startFacade(){
 		Login login = new Login();
 		UserType = login.login();
+		boolean flag = true;
 		System.out.println("User is : " + UserType);
 		if(UserType == 0){
 			System.out.println("User is Buyer\n");
-		}else {
+		}else if (UserType == 1){
 			System.out.println("User is Seller\n");
 		}
+		else {
+			System.out.println("Wrong input");
+			flag = false;
+		}
 
-		while(true){
-			System.out.println("Select Product from following : \n 0. Meat Product \n 1. Produce Product");
-			Scanner sn = new Scanner(System.in);
-			String inp1 = sn.nextLine();
-			if (inp1.equalsIgnoreCase("meat product") || inp1.equalsIgnoreCase("0")){
-				nProductCategory = 0;
-				break;
-			}else if (inp1.equalsIgnoreCase("produce product") || inp1.equalsIgnoreCase("1")){
-				nProductCategory = 1;
-				break;
-			}else {
-				System.out.println("Wrong Input Selected, select again");
+		if (flag){
+			while(true){
+				System.out.println("Select Product from following : \n 0. Meat Product \n 1. Produce Product");
+				Scanner sn = new Scanner(System.in);
+				String inp1 = sn.nextLine();
+				if (inp1.equalsIgnoreCase("meat product") || inp1.equalsIgnoreCase("0")){
+					nProductCategory = 0;
+					break;
+				}else if (inp1.equalsIgnoreCase("produce product") || inp1.equalsIgnoreCase("1")){
+					nProductCategory = 1;
+					break;
+				}else {
+					System.out.println("Wrong Input Selected, select again");
 
+				}
+	//			System.out.println("Visitor pattern Started ......");
 			}
-//			System.out.println("Visitor pattern Started ......");
-		}
-		Person person;
-//		System.out.println("\nBridge Pattern implemented .... \n");
-		if(UserType == 0){
-			person = new Buyer();
-		}else {
-			person = new Seller();
-		}
-		ProductMenu productMenu ;
-		if(nProductCategory == 0){
-			productMenu = new MeatProductMenu();
-		}else {
-			productMenu = new ProduceProductMenu();
-		}
+			Person person;
+	//		System.out.println("\nBridge Pattern implemented .... \n");
+			if(UserType == 0){
+				person = new Buyer();
+			}else {
+				person = new Seller();
+			}
+			ProductMenu productMenu ;
+			if(nProductCategory == 0){
+				productMenu = new MeatProductMenu();
+			}else {
+				productMenu = new ProduceProductMenu();
+			}
 
-		System.out.println("\nVisitor Pattern implemented ......\n");
-		remind();
-		System.out.println("\nIterator Pattern implemented .......\n");
-//		productMenu.showMenu();
-		ClassProductList productList = new ClassProductList(productMenu);
-		Iterator iterator = productList.createIterator();
-		ProductIterator productIterator = new ProductIterator();
-		System.out.println("\n Menu is : ");
-		while(productIterator.hasNext(iterator)){
-			System.out.println(productIterator.Next(iterator));
+			System.out.println("\nVisitor Pattern implemented ......\n");
+			remind();
+			System.out.println("\nIterator Pattern implemented .......\n");
+	//		productMenu.showMenu();
+			ClassProductList productList = new ClassProductList(productMenu);
+			Iterator iterator = productList.createIterator();
+			ProductIterator productIterator = new ProductIterator();
+			System.out.println("\n Menu is : ");
+			while(productIterator.hasNext(iterator)){
+				System.out.println(productIterator.Next(iterator));
+			}
+
 		}
 
 
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # PTBS
 Author: Jay Sachin Chiddarwar
 Product Trading and Bidding System
+
+Step 1: Run the DriverClass.java to run driver Code (DriverClass contain call to Facade)
+
+Step 2: Follow instruction on console for appropriate output.
+
+-> Note :
+To change txt files please replace with same name and same location as in this project. Else it will give FileNotFoundException.
